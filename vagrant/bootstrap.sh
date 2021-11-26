17cebb7b81394acf15ed55f4f2c98bdaa368f915
Paulo Kinjo <paulokinjo@hotmail.com>
2021-11-26 20:47:36 +0900

    Update to use containerd when provisioning K8 cluster

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -2,12 +2,68 @@
 
 echo "[TASK 1] Update /etc/hosts file"
 cat >>/etc/hosts<<EOF
-192.168.33.10   master
-192.168.33.11   worker1
-192.168.33.12   worker2
+192.168.56.10   master
+192.168.56.11   worker1
+192.168.56.12   worker2
 EOF
 
-echo "[TASK 2] Install Docker"
+echo "[TASK 2] Starts containerd configuration"
+
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+echo "[TASK 3] Setup required sysctl params, these persist across reboots."
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+echo "[TASK 4] Apply sysctl params without reboot"
+sudo sysctl --system
+
+echo "[TASK 5] Set up the repository"
 sudo apt-get update
-curl https://releases.rancher.com/install-docker/20.10.sh | sh
-sudo usermod -aG docker vagrant
+sudo apt-get install -y \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
+echo "[TASK 6] Install containerd"
+sudo apt-get update
+sudo apt-get install containerd.io -y
+
+echo "[TASK 7] Configure containerd"
+sudo mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
+
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+
+echo "[TASK 8] Update and install apt-transport-https"
+sudo apt-get update
+sudo apt-get install -y apt-transport-https 
+
+echo "[TASK 9] Download the Google Cloud public signing key"
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+echo "[TASK 10] Add the Kubernetes apt repository"
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+echo "[TASK 11] Install Kubelet and Kubeadm"
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm
+sudo apt-mark hold kubelet kubeadm
--- vagrant/bootstrap_master.sh
+++ vagrant/bootstrap_master.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-echo "[TASK 1] Install RKE Rancher"
-wget https://github.com/rancher/rke/releases/download/v1.3.1/rke_linux-amd64
-mv rke_linux-amd64 rke
-chmod +x rke
-sudo mv rke /usr/sbin/
+echo "[TASK 1]-[Master] Install Kubectl"
+sudo apt-get update
+sudo apt-get install -y kubectl
+sudo apt-mark hold kubectl
 
+echo "[TASK 2]-[Master] Pull images for kubeadm setup"
+sudo kubeadm config images pull
 
-echo "[TASK 2] Install Kubectl"
-sudo snap install kubectl --classic
-mkdir -p ~/.kube
-docker version
+echo "[TASK 3]-[Master] Initialize cluster"
+sudo kubeadm init --apiserver-advertise-address=192.168.56.10 --pod-network-cidr=192.168.0.0/16
+
+echo "[TASK 4]-[Master] Configure kubeconfig"
+sudo mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+echo "[TASK 5]-[Master] Initialize Calico CNI"
+kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
+kubectl create -f https://docs.projectcalico.org/manifests/custom-resources.yaml
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
+echo "[TASK 6]-[Master] List all nodes"
+kubectl get nodes -o wide
+
