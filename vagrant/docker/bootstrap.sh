7d82778cc912a13b906fd2e6d82a7a49b7c6ee8b
Paulo Kinjo <paulo.kinjo@paypay-corp.co.jp>
2022-05-09 16:32:19 +0900

    (feat): new cluster config

--- vagrant/docker/bootstrap.sh
+++ vagrant/docker/bootstrap.sh
@@ -14,3 +14,20 @@ sudo -u vagrant dockerd-rootless-setuptool.sh install
 
 echo "[TASK 4] Add current user to docker group"
 sudo usermod -aG docker vagrant
+
+echo "[TASK 5] Install build-essential"
+sudo apt-get install build-essential cmake -y
+
+echo "[TASK 6] Install NVM"
+sudo -u vagrant curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "/home/vagrant/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+echo "[TASK 7] Install Node 14"
+nvm install 14
+
+echo "[TASK 8] Install Yarn"
+npm install -g yarn
+
+echo "[TASK 9] Install Python"
+sudo apt-get install python -y
